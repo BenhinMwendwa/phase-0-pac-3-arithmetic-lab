0da8eaa13391671a1183bf464233ce1949f75ff8
Benhin <benhinmwendwa7@gmail.com>
2024-03-15 02:10:46 +0300

    calc

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a , b){
+    return a + b;
+}
+
+function subtract(a , b){
+    return a - b;
+    
+}
+function multiply(a , b){
+    return a * b ;
+    
+}
+function divide(a , b){
+    return a / b ;
+} 
+
+function increment(n){
+    return n+=1;
+}
+console.log(increment(470));
+function decrement(n){
+    return n-=1;
+}
+console.log(increment(178));
+
+function makeInt(n){
+return parseInt(n, 10);
+}
+
+console.log(makeInt(1));
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+console.log(preserveDecimal(2.111));
+
